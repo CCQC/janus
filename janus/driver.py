4df2b4666c6cf63585d14592f05db4849bc22e39
Boyi Zhang <bz11252@uga.edu>
2017-12-14 13:09:26 -0500

    modified the way driver gets energy

--- janus/driver.py
+++ janus/driver.py
@@ -4,8 +4,9 @@ This is the driver module
 from . import parser
 from .system import System
 
-def energy():
+def energy(input_file):
     
     # Initialize system
     sys = parser.parse_input(input_file)
+    sys.make_qm_molecule()
     sys.get_qmmm_energy()
