60d4067527a1728d1dbaad01c33f1769fd0f858a
Boyi Zhang <bz11252@uga.edu>
2018-07-23 16:54:27 -0400

    removed the need for a systems object in the qmmm class

--- janus/qmmm.py
+++ janus/qmmm.py
@@ -4,11 +4,12 @@ QMMM class for QMMM computations
 """
 class QMMM(object):
 
-    def __init__(self, mm_wrapper, qm_wrapper, system):
+    def __init__(self, mm_wrapper, qm_wrapper):
         
         self.mm_wrapper = mm_wrapper
         self.qm_wrapper = qm_wrapper
-        self.system = system
+        self.qm_atoms = mm_wrapper._system.qm_atoms
+        self.boundary_treatment = mm_wrapper._system.boundary_treatment
         
     def additive(self):
         """
@@ -16,117 +17,101 @@ class QMMM(object):
         a qm/mm energy with a specified embedding method using
         an additive scheme
         """
-        self.mm_wrapper = mm_wrapper
-        self.qm_wrapper = mm_wrapper
-        self.system = system
 
-        #need to add if these things are none then do the following
+        #need to add if these things are none then do the following?
+        # maybe not because already checks in mm_wrapper functions
 
         # Get MM energy on MM region
-        if not system.second_subsys:
-            system.second_subsys = mm_wrapper.get_second_subsys()
+        self.second_subsys = mm_wrapper.get_second_subsys()
 
         # Get non coulomb MM energy on PS-SS interaction
-        if not system.boundary:
-            system.boundary = mm_wrapper.get_boundary(coulomb=False)
+        self.boundary = mm_wrapper.get_boundary(coulomb=False)
 
         # Get any link atom information
-        if not system.boundary_info:
-            system.boundary_info = mm_wrapper.get_boundary_info()
+        self.boundary_info = mm_wrapper.get_boundary_info()
 
         # Get QM energy
-        if not system.qm:
-            # get QM positions from pdb
-            if system.qm_positions == None:
-                system.qm_positions = mm_wrapper.get_qm_positions() 
-            system.qm = qm_wrapper.get_qm()
+        # get QM positions from pdb
+        if qm_positions == None:
+            qm_positions = mm_wrapper.get_qm_positions() 
+        self.qm = qm_wrapper.get_qm(qm_positions)
 
         # Compute total QM/MM energy based on additive scheme
-        system.qmmm_energy = system.second_subsys['energy']\
-                            + system.boundary['energy']\
-                            + system.qm['energy']
+        self.qmmm_energy = self.second_subsys['energy']\
+                      + self.boundary['energy']\
+                      + self.qm['energy']
 
-    def subtractive(system):
+        # Compute QM/MM gradients 
+        qmmm_gradients = self.compute_gradients(scheme='additive')
+
+    def subtractive(system, entire_sys=None):
         """
         Gets energies of needed components and computes
         a qm/mm energy with a subtractive mechanical embedding scheme
         """
-        self.mm_wrapper = mm_wrapper
-        self.qm_wrapper = mm_wrapper
-        self.system = system
-
-        mm_wrapper, qm_wrapper = initialize_wrappers(system)
 
         # Get MM energy on whole system
-        if not system.entire_sys:
-            system.entire_sys = mm_wrapper.get_entire_sys()
+        if not entire_sys:
+            self.entire_sys = mm_wrapper.get_entire_sys()
 
         # Get MM energy on QM region
-        if not system.primary_subsys:
-            system.primary_subsys = mm_wrapper.get_primary_subsys(link=True)
+        self.primary_subsys = mm_wrapper.get_primary_subsys(link=True)
 
-        # Get any link atom information
-        if not system.boundary_info:
-            system.boundary_info = mm_wrapper.get_boundary_info()
+        # Get position and identity of link atom for QM computation if relevant
+        self.boundary_info = mm_wrapper.get_boundary_info()
 
         # Get QM energy
-        if not system.qm:
-            # get QM positions from pdb
-            if system.qm_positions == None:
-                system.qm_positions = mm_wrapper.get_qm_positions() 
-            system.qm = qm_wrapper.get_qm()
+        # get QM positions from pdb
+        if qm_positions == None:
+            qm_positions = mm_wrapper.get_qm_positions() 
+        self.qm = qm_wrapper.get_qm(qm_positions)
 
         # Compute the total QM/MM energy based on
         # subtractive Mechanical embedding
-        system.qmmm_energy = system.entire_sys['energy']\
-                            - system.primary_subsys['energy']\
-                            + system.qm['energy']
+        self.qmmm_energy = self.entire_sys['energy']\
+                      - self.primary_subsys['energy']\
+                      + self.qm['energy']
 
         # Compute QM/MM gradients 
-        system.qmmm_gradients = self.compute_gradients(scheme='subtractive')
+        self.qmmm_gradients = self.compute_gradients(scheme='subtractive')
 
 
     def compute_gradients(self, scheme = 'subtractive'):
 
         if scheme == 'subtractive':
 
-            all_mm_grad, ps_mm_grad, qm_grad = self.system.entire_sys['gradients'], self.system.primary_subsys['gradients'], self.system.qm['gradients']
+            all_mm_grad, ps_mm_grad, qm_grad = self.entire_sys['gradients'], self.primary_subsys['gradients'], self.qm['gradients']
             qmmm_grad = deepcopy(all_mm_grad)
                 
             # iterate over list of qm atoms
-            for i, atom in enumerate(self.system.qm_atoms):
+            for i, atom in enumerate(self.qm_atoms):
 
                 # compute the qmmm gradient for the qm atoms: 
                 # mm_entire - mm_primary - qm
                 qmmm_grad[atom] += - ps_mm_grad[i] + qm_grad[i]
                 
-                if self.system.boundary_info:
-                    q1 = int(self.system.boundary_info[0]['qm_id']) - 1
-                    m1 = int(self.system.boundary_info[0]['mm_id']) - 1
-                    g = self.system.boundary_info[0]['g_factor']
+                if self.boundary_info:
+                    q1 = int(self.boundary_info[0]['qm_id']) - 1
+                    m1 = int(self.boundary_info[0]['mm_id']) - 1
+                    g = self.boundary_info[0]['g_factor']
                     if atom == q1:
-                        if self.system.boundary_treatment == 'link_atom':
+                        if self.boundary_treatment == 'link_atom':
                             # Project forces of link atoms onto the mm and qm atoms of the link atom bond
                             qmmm_grad[atom] += -(1 - g) * ps_mm_grad[-1] + (1 - g) * qm_grad[-1]
                             qmmm_grad[m1] += -g * ps_mm_grad[-1] + g * qm_grad[-1]
-
-                        if self.system.boundary_treatment == 'RC' or self.system.boundary_treatment == 'RCD':
+                            
+                        # Forces on M2 requires forces on point charges which I'm not sure about so need to double check
+                        if self.boundary_treatment == 'RC' or self.boundary_treatment == 'RCD':
                             qmmm_grad[atom] += -(1 - g) * ps_mm_grad[-1] + (1 - g) * qm_grad[-1]
                             qmmm_grad[m1] += -g * ps_mm_grad[-1] + g * qm_grad[-1]
 
             
         if scheme == 'additive':
-            pass
+            print("Gradients for additive scheme not available yet") 
 
         return qmmm_grad
         
-
-    def get_system(self):
-
-        return self.system
-
-
-    def compute_force(self, scheme):
-        
+    def get_info(self, scheme, system, partition=None):
         pass
+
 
