55f19b52d47a5a3799f68949e55ef432676e860a
Boyi Zhang <bz11252@uga.edu>
2018-07-23 16:22:17 -0400

    initial file for aqmmm with empty functions

---
+++ janus/aqmmm.py
@@ -0,0 +1,16 @@
+"""
+AQMMM class for adaptive QMMM computations
+"""
+class AQMMM(object):
+
+    def __init(self):
+        pass
+
+    def partition(self):
+        pass
+
+    def save(self):
+        pass
+
+    def get_info(self):
+        pass
