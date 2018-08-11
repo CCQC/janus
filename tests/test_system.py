473ba85316fc4f8937a9deb69c555451eccaa06f
Boyi Zhang <bz11252@uga.edu>
2018-08-11 16:04:58 -0400

    added testing for partition class

--- tests/test_system.py
+++ tests/test_system.py
@@ -40,3 +40,21 @@ def test_compute_scale_factor_g():
     assert g1 == 0.7133333333333334
     assert g2 == 0.7328767123287672
 
+
+def test_compute_COM():
+
+    part = system.Partition([0,1,2], 'qm')
+
+    coord = [['O', [0.123, 3.593, 5.841]],
+        ['H', [-0.022, 2.679, 5.599]],
+        ['H', [0.059, 3.601, 6.796]]]
+
+    xyz = part.compute_COM(coord)
+    com = np.array([0.11130575, 3.54230624, 5.88089475])
+    
+    assert np.allclose(part.qm_atoms, np.array([0,1,2]))
+    assert part.ID == 'qm'
+    assert not part.switching_functions
+    assert np.allclose(xyz, com)
+
+        
