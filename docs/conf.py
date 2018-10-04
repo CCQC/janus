12a552149202135fd3fe780b264ba0d6521256de
Boyi Zhang <bz11252@uga.edu>
2018-10-04 13:33:42 -0400

    updating docs build

--- docs/conf.py
+++ docs/conf.py
@@ -78,7 +78,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphix_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
